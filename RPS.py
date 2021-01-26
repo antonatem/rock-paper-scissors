17089aae43c398b250315587cd00c3af4bc87b90
antonatem <natemaritim@gmail.com>
2021-01-26 16:28:56 +0100

    Main player play against bots

--- RPS.py
+++ RPS.py
@@ -1,73 +1,33 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 8,
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "[[0. 0. 0.]\n",
-      " [0. 0. 0.]\n",
-      " [0. 0. 0.]\n",
-      " [0. 0. 0.]]\n"
-     ]
+play_order = {}
+def player(prev_play, opponent_history=[]):
+    ideal_response = {'P': 'S', 'R': 'P', 'S': 'R'}
+    guess = 'P'
+
+    if not prev_play:
+        prev_play = 'R'
+    opponent_history.append(prev_play)
+
+    last_three = ''.join(opponent_history[-3:])
+    last_four = ''.join(opponent_history[-4:])
+    if len(last_four) == 4:
+      if play_order.get(last_four) is not None:
+        play_order[last_four] += 1
+      else:
+        play_order[last_four] = 1
+
+    potential_plays = [
+        last_three + "R",
+        last_three + "P",
+        last_three + "S"
+    ]
+
+    sub_order = {
+        k: play_order.get(k)
+        for k in potential_plays if k in play_order
     }
-   ],
-   "source": [
-    "import numpy as np\n",
-    "\n",
-    "# def guess():\n",
-    "ACTION = ['R', 'P', 'S']\n",
-    "Q = np.zeros((4, 3))\n",
-    "\n",
-    "# Define hyperparameters\n",
-    "episodes = 1500\n",
-    "learning_rate = 0.2\n",
-    "gamma = 0.9\n",
-    "\n",
-    "# Define exploration parameters\n",
-    "epsilon = 0.9\n",
-    "\n",
-    "if np.random.uniform(0, 1) < epsilon:\n",
-    "    np.random.choice(ACTION, 1)\n",
-    "else:\n",
-    "    np.argmax(Q[state, :])\n",
-    "\n",
-    "# Q[state, action] = Q[state, action] + LEARNING_RATE * (reward + GAMMA * np.max(Q[new_state, :]) - Q[state, action])\n",
-    "\n",
-    "print(Q)"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.8.2"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 4
-}
+
+    if len(sub_order) > 0:
+      prediction = max(sub_order, key=sub_order.get)[-1:]
+      guess = ideal_response[prediction]
+
+    return guess
