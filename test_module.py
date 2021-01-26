bcde60d8279af098e1a0f22f0f5e79ac0ab52da5
antonatem <natemaritim@gmail.com>
2021-01-26 16:30:25 +0100

    Unit tests for player vs bots score being over 60%

--- test_module.py
+++ test_module.py
@@ -1,71 +1,38 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "import unittest\n",
-    "from RPS_game import play, mrugesh, abbey, quincy, kris\n",
-    "from RPS import player\n",
-    "\n",
-    "\n",
-    "class UnitTests(unittest.TestCase):\n",
-    "    print()\n",
-    "\n",
-    "    def test_player_vs_quincy(self):\n",
-    "        print(\"Testing game against quincy...\")\n",
-    "        actual = play(player, quincy, 1000) >= 60\n",
-    "        self.assertTrue(\n",
-    "            actual,\n",
-    "            'Expected player to defeat quincy at least 60% of the time.')\n",
-    "\n",
-    "    def test_player_vs_abbey(self):\n",
-    "        print(\"Testing game against abbey...\")\n",
-    "        actual = play(player, abbey, 1000) >= 60\n",
-    "        self.assertTrue(\n",
-    "            actual,\n",
-    "            'Expected player to defeat abbey at least 60% of the time.')\n",
-    "\n",
-    "    def test_player_vs_kris(self):\n",
-    "        print(\"Testing game against kris...\")\n",
-    "        actual = play(player, kris, 1000) >= 60\n",
-    "        self.assertTrue(\n",
-    "            actual, 'Expected player to defeat kris at least 60% of the time.')\n",
-    "\n",
-    "    def test_player_vs_mrugesh(self):\n",
-    "        print(\"Testing game against mrugesh...\")\n",
-    "        actual = play(player, mrugesh, 1000) >= 60\n",
-    "        self.assertTrue(\n",
-    "            actual,\n",
-    "            'Expected player to defeat mrugesh at least 60% of the time.')\n",
-    "\n",
-    "\n",
-    "if __name__ == \"__main__\":\n",
-    "    unittest.main()\n"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.8.2"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 4
-}
+import unittest
+from RPS_game import play, mrugesh, abbey, quincy, kris
+from RPS import player
+
+
+class UnitTests(unittest.TestCase):
+    print()
+
+    def test_player_vs_quincy(self):
+        print("Testing game against quincy...")
+        actual = play(player, quincy, 1000) >= 60
+        self.assertTrue(
+            actual,
+            'Expected player to defeat quincy at least 60% of the time.')
+
+    def test_player_vs_abbey(self):
+        print("Testing game against abbey...")
+        actual = play(player, abbey, 1000) >= 60
+        self.assertTrue(
+            actual,
+            'Expected player to defeat abbey at least 60% of the time.')
+
+    def test_player_vs_kris(self):
+        print("Testing game against kris...")
+        actual = play(player, kris, 1000) >= 60
+        self.assertTrue(
+            actual, 'Expected player to defeat kris at least 60% of the time.')
+
+    def test_player_vs_mrugesh(self):
+        print("Testing game against mrugesh...")
+        actual = play(player, mrugesh, 1000) >= 60
+        self.assertTrue(
+            actual,
+            'Expected player to defeat mrugesh at least 60% of the time.')
+
+
+if __name__ == "__main__":
+    unittest.main()
